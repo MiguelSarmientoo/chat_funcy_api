9bc29c37324a1ba20a620b34e03c5f6f733c65dd
XxDavexX <dave.santivanez@gmail.com>
2024-08-12 22:22:56 -0500

    avance db

--- config/database.js
+++ config/database.js
@@ -8,17 +8,33 @@
 // module.exports = sequelize;
 
 
+// const { Sequelize } = require('sequelize');
+// require('dotenv').config();  // Asegúrate de cargar las variables de entorno
+
+// const sequelize = new Sequelize(
+//   process.env.MYSQL_DATABASE,    // Nombre de la base de datos
+//   process.env.MYSQL_USER,        // Usuario de la base de datos
+//   process.env.MYSQL_PASSWORD,    // Contraseña de la base de datos
+//   {
+//     host: process.env.MYSQL_HOST, // Host de la base de datos
+//     dialect: 'mysql',            // Dialecto de la base de datos
+//     port: process.env.MYSQL_PORT  // Puerto de la base de datos
+//   }
+// );
+
+// module.exports = sequelize;
+
 const { Sequelize } = require('sequelize');
 require('dotenv').config();  // Asegúrate de cargar las variables de entorno
 
 const sequelize = new Sequelize(
-  process.env.MYSQL_DATABASE,    // Nombre de la base de datos
-  process.env.MYSQL_USER,        // Usuario de la base de datos
-  process.env.MYSQL_PASSWORD,    // Contraseña de la base de datos
+  'chat_app',    // Nombre de la base de datos
+  'admin',        // Usuario de la base de datos
+  'Dryan250303',    // Contraseña de la base de datos
   {
-    host: process.env.MYSQL_HOST, // Host de la base de datos
+    host: 'database-1.ctkki2usirrr.us-east-2.rds.amazonaws.com', // Host de la base de datos
     dialect: 'mysql',            // Dialecto de la base de datos
-    port: process.env.MYSQL_PORT  // Puerto de la base de datos
+    port: '3306'  // Puerto de la base de datos
   }
 );
 
